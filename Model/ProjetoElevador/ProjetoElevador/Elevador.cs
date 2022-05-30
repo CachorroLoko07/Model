e88c301ac21eedfdabcf3e536ed621833e71ba42
Ezequiel Dos Santos <91284995+CachorroLoko07@users.noreply.github.com>
2022-05-30 00:37:44 -0300

    Add files via upload

---
+++ Model/ProjetoElevador/ProjetoElevador/Elevador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador
+{
+    public class Elevador
+    {
+       public int terreo = 0;
+       public int total_andares=10;
+       public int capacidade = 5;
+
+      //  int Inicializar()
+       // {
+         //   capacidade = 5;
+           // return total_andares;   
+        
+        }
+    }
+
+
---
+++ Model/ProjetoElevador/ProjetoElevador/elevador1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoElevador
+{
+    internal class elevador
+    {
+        internal void getInputFromUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
